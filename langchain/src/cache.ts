46f6462b8aeb446d7896f6fc734a43ec9c7b28f7
Paul Vanderspek <paulvanderspek@gmail.com>
2023-03-14 20:36:11 -0700

    use alternate crypto

--- langchain/src/cache.ts
+++ langchain/src/cache.ts
@@ -1,4 +1,4 @@
-import crypto from "node:crypto";
+import { sha256 } from "hash.js";
 import type { RedisClientType } from "redis";
 import { Generation } from "./schema/index.js";
 
@@ -15,7 +15,7 @@ import { Generation } from "./schema/index.js";
 
 const getCacheKey = (prompt: string, llmKey: string, idx?: string): string => {
   const key = `${prompt}_${llmKey}${idx ? `_${idx}` : ""}`;
-  return crypto.createHash("sha256").update(key).digest("hex");
+  return sha256().update(key).digest("hex");
 };
 
 export abstract class BaseCache<T = Generation[]> {
